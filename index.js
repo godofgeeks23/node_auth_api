ff057f79501397c11c1751803485051d905662de
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-09-23 00:12:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const authroute = require('./routes/auth');
 
 dotenv.config()
 
@@ -8,7 +9,8 @@ const app = express();
 
 mongoose.connect(process.env.DB_CONNECT, () => console.log("connected to DB"));
 
-const authroute = require('./routes/auth');
+app.use(express.json());
+
 
 // route middleware
 app.use('/api/user', authroute)
